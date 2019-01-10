938c0daccc296059d6cfb8e636bbb6665a56bbf8
Victor Jimenez <betabandido@gmail.com>
2019-01-10 22:39:24 +0100

    Add MongoDB-based repository to the C# implementation

--- csharp/RestApi/RestApi/Startup.cs
+++ csharp/RestApi/RestApi/Startup.cs
@@ -16,7 +16,7 @@ namespace RestApi
                 .AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             
-            services.AddSingleton<IValueRepository, InMemoryValueRepository>();
+            services.AddSingleton<IValueRepository>(ValueRepositoryFactory.Create());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
