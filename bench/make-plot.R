3e2cbdceffd583acc81761e69cec66122f294278
Victor Jimenez <betabandido@gmail.com>
2019-09-15 22:38:15 +0200

    Add zoom-in plot for latency

--- bench/make-plot.R
+++ bench/make-plot.R
@@ -14,6 +14,8 @@ keepalive <- args[2]
 
 dt <- fread(sprintf('results-%s-keepalive:%s.csv', implementation, keepalive))
 
+# QPS plot
+
 pl <- ggplot(dt, aes(`requested-qps`, `actual-qps`)) +
   geom_line() +
   geom_point() +
@@ -22,6 +24,8 @@ pl <- ggplot(dt, aes(`requested-qps`, `actual-qps`)) +
   ylim(0, max(dt[, `requested-qps`]))
 ggsave(sprintf('qps-%s-keepalive:%s.png', implementation, keepalive), 
        pl, width = 5, height = 4, units = "in")
+
+# latency plot
 
 dt[, c('actual-qps', 'total-count', 'success-count') := NULL]
 dt <- melt(dt, 
@@ -36,4 +40,17 @@ pl <- ggplot(dt, aes(`requested-qps`, latency, color = metric)) +
   ylab('Latency (ms)') +
   ylim(0, NA)
 ggsave(sprintf('latency-%s-keepalive:%s.png', implementation, keepalive),
+       pl, width = 5, height = 4, units = "in")
+
+# latency plot (w/o 99th percentile)
+
+dt <- dt[metric != 'latency-99.9']
+
+pl <- ggplot(dt, aes(`requested-qps`, latency, color = metric)) +
+  geom_line() +
+  geom_point() +
+  xlab('Requested QPS') +
+  ylab('Latency (ms)') +
+  ylim(0, NA)
+ggsave(sprintf('zoom-latency-%s-keepalive:%s.png', implementation, keepalive),
        pl, width = 5, height = 4, units = "in")
